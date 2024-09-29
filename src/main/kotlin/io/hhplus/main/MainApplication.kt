b3def3d454e6d5c4081fe0368e9038c124f1fefe
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-09-29 17:25:04 +0900

    init project

---
+++ src/main/kotlin/io/hhplus/main/MainApplication.kt
@@ -0,0 +1,11 @@
+package io.hhplus.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MainApplication
+
+fun main(args: Array<String>) {
+    runApplication<MainApplication>(*args)
+}
